b2d2147059f343fa82e0d4126b4ae645904a8495
rvladimir001 <32569748+rvladimir001@users.noreply.github.com>
2018-01-31 21:30:42 +0300

    Update 01 list.py

--- lesson 06/01 list.py
+++ lesson 06/01 list.py
@@ -1,4 +1,10 @@
-listNumber =[2, 34, 56, 7, 19, 6 ]
+import random
+listNumber =[]
+i=6
+while i>0:
+    x = random.randrange(0, 99, 1)
+    listNumber.append(x)
+    i=i-1
 print(listNumber)#список как есть
 for i in listNumber:#вывод чере цикл
     print(i)
