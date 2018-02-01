805ad61ec70eba17c1dea452c4feb9fa9e95e5e5
rvladimir001 <32569748+rvladimir001@users.noreply.github.com>
2018-02-01 14:39:06 +0000

    Update 07 list.py

--- lesson 06/07 list.py
+++ lesson 06/07 list.py
@@ -1,12 +1,13 @@
-listNumber =[7, 15, 4, 2, 4, 12, 7, 9, 8, 5]
+import random
+listNumber =[]
+k=10
+while k>0:
+    x = random.randrange(0, 99, 1)
+    listNumber.append(x)
+    k=k-1
+print(listNumber)
+
 for i in listNumber:
     if(i%3!=0):
         listNumber.remove(i)
 print(listNumber)
-
-#i=len(listNumber)
-#while i>0:
-#    if(i%3!=0):
-#        del listNumber[i]
-#        i=i-1
-#print(listNumber)
