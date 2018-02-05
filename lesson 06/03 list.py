d8d500c9f0ccc095510028fea220755016962f5d
rvladimir001 <32569748+rvladimir001@users.noreply.github.com>
2018-02-05 19:31:30 +0300

    сделал второй вариант с for

--- lesson 06/03 list.py
+++ lesson 06/03 list.py
@@ -1,3 +1,7 @@
+#Заполнить список из шести элементов произвольными целочисленными значениями.
+#Вывести список на экран в обратной последовательности. Два варианта получения
+#обратной последовательности: с приминением цикла и без него.
+
 import random
 listNumber =[]
 k=6
@@ -17,3 +21,15 @@ print(NewListNumber)
 
 listNumber.reverse()
 print(listNumber)
+
+# 2 вариант с for
+import random
+listNumber =[]
+NewListNumber=[]
+for i in range(1, 7):
+    x = random.randrange(0, 100, 1)
+    listNumber.append(x)
+print(listNumber)
+listNumber.reverse()
+print(listNumber)
+
