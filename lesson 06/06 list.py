9806229c9ec6d65160e7a7daab8a6a2bc74264bb
rvladimir001 <32569748+rvladimir001@users.noreply.github.com>
2018-02-05 19:49:41 +0300

    второй вариант с библиотекой math

--- lesson 06/06 list.py
+++ lesson 06/06 list.py
@@ -1,8 +1,20 @@
+#Заполнить список из шести элементов квадратными корнями
+#произвольныз целочисленныз значений.
+#Вывести список на экран через запятую.
+
 import random
+import math
 listNumber =[]
+WListNumber =[]
 i=10
 while i>0:
     x = random.randrange(0, 99, 1)
     listNumber.append(round(x**0.5))
     i=i-1
 print(listNumber)
+
+#второй вариант с библиотекой math
+for j in range(1, 11):
+    y = random.randrange(0, 99, 1)
+    WListNumber.append(round(math.sqrt(y)))
+print(WListNumber)
