3ee8a9f1fbbfbb6de49176410c2b917fe4d2b3b3
rvladimir001 <32569748+rvladimir001@users.noreply.github.com>
2018-02-08 19:55:08 +0300

    Add files via upload

---
+++ lesson 07/06 lesson.py
@@ -0,0 +1,29 @@
+#Работаем с чужим кодом. В коде из файла
+#https://github.com/groall/python_learning/blob/master/lesson_04_cycles/task03.py
+#нужно заменить повторяющееся с циклах действие суммирования на вызов функции,
+#функция должна работать с глобальной переменной sumNumbers.
+
+# Пользователь вводит любое целое положительное число (сделать проверку).
+# Программа должна просуммировать все числа от 1 до введенного пользователем числа (не включая его).
+
+x = input('Введите целое положительное число: ')
+
+sumNumbers = 0
+
+def cycle(a):
+    global sumNumbers
+    
+    if not a.isdigit():
+        print("Вы ввели не число")
+        exit()
+
+    xInt = int(a)
+    if xInt < 1:
+        print("Число не положительное")
+        exit()
+    
+    for i in range(1, xInt+1):
+        sumNumbers += i
+    print("Сумма", sumNumbers)
+    
+cycle(x)
